737274ab4a0470627034d119a240998dc6215570
Kan Luo <lkklcozy@gmail.com>
2020-06-23 15:38:08 -0600

    ✨ feat: integrate the gatsby-theme-code-notes

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # code-notes
+
+![](https://www.elegantthemes.com/blog/wp-content/uploads/2016/11/code-snippets.jpg)
+
 Personal code-related notes and snippets
+
+This application was built with [gatsby-theme-code-notes](https://github.com/mrmartineau/gatsby-theme-code-notes)
---
+++ gatsby-config.js
@@ -0,0 +1,23 @@
+module.exports = {
+  siteMetadata: {
+    title: "Lkcozy Code Notes",
+    description: `Personal code-related code notes and snippets`,
+    author: "Kan Luo",
+  },
+  plugins: [
+    {
+      resolve: "gatsby-theme-code-notes",
+      options: {
+        contentPath: "notes",
+        basePath: "/",
+        showThemeInfo: false,
+        showDescriptionInSidebar: true,
+        gitRepoContentPath:
+          "https://github.com/lkcozy/code-notes/tree/master/notes/",
+        openSearch: {
+          siteUrl: "http://lkcozy.github.io/code-notes",
+        },
+      },
+    },
+  ],
+};
